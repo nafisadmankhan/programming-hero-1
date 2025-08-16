f6944a8e56381846cd8bd10272205b3c088a266e
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-08-16 16:50:16 +0600

    Updated

--- ms04/mod20/js-problems-part1/convert.js
+++ ms04/mod20/js-problems-part1/convert.js
@@ -8,9 +8,23 @@ function inchToFeet(inch) {
 function inchToFeetExtended(inch){
     const feetFraction = inch / 12;
     const feetNumber = parseInt(feetFraction);
+    const inchRemaining = inch % 12; 
+    const result = feetNumber + ' ft ' + inchRemaining + ' inch '
+    return result;
 }
 
 const johnHeight = inchToFeet(75);
 console.log(johnHeight);
 
 const johnHeighExtended = inchToFeetExtended(75);
+console.log(johnHeighExtended);
+
+function mileToKilometer(mile) {
+    const kilo = milo * 1.60934;
+    return kilo;
+}
+
+function kilometerToMile(kilo) {
+    const mile = kilo * 0.621371;
+    return mile;
+}
