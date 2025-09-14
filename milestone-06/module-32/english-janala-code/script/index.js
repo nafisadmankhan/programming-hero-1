5c564554d9509b8693f4702de403b1d5e5256011
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-09-14 19:12:29 +0600

    Added

--- milestone-06/module-32/english-janala-code/script/index.js
+++ milestone-06/module-32/english-janala-code/script/index.js
@@ -1,5 +1,29 @@
 // console.log('Hello World')
 
+const createElements = (arr) => {
+    const htmlElements = arr.map((el) => 
+        `<span class="btn">${el}</span>`
+    )
+   return htmlElements.join(" ")
+}
+
+function pronounceWord(word) {
+    const utterance = new SpeechSynthesisUtterance(word);
+    utterance.lang = "en-EN"; // English
+    window.speechSynthesis.speak(utterance);
+}
+
+const manageSpinner = (status) => {
+    if (status==true){
+        document.getElementById("spinner").classList.remove("hidden")
+        document.getElementById("word-container").classList.add("hidden")
+    } else {
+        document.getElementById("word-container").classList.remove("hidden")
+        document.getElementById("spinner").classList.add("hidden")
+
+    }
+}
+
 const loadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response
     .then(res => res.json()) // promise of json
@@ -16,6 +40,7 @@ const removeActive = () => {
 
 const loadLevelWord = (id) => {
     // console.log(id)
+    manageSpinner(true)
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
         .then((res) => res.json())
@@ -38,8 +63,45 @@ const loadWordDetail = async(id) => {
     displayWordDetails(details.data)
 }
 
+// {
+//     "word": "Eager",
+//     "meaning": "আগ্রহী",
+//     "pronunciation": "ইগার",
+//     "level": 1,
+//     "sentence": "The kids were eager to open their gifts.",
+//     "points": 1,
+//     "partsOfSpeech": "adjective",
+//     "synonyms": [
+//         "enthusiastic",
+//         "excited",
+//         "keen"
+//     ],
+//     "id": 5
+// }
+
 const displayWordDetails = (word) => {
     console.log(word)
+    const detailsBox = document.getElementById("details-container")
+    detailsBox.innerHTML = `
+    <div class="">
+        <h2 class="text-2xl font-bold">
+            ${word.word} (<i class="fa-solid fa-microphone-lines"></i>:${word.pronunciation})
+        </h2>
+    </div>
+    <div class="">
+        <h2 class="font-bold">Meaning</h2>
+        <p>${word.meaning}</p>
+    </div>
+    <div class="">
+        <h2 class="font-bold">Example</h2>
+        <p>${word.sentence}</p>
+    </div>
+    <div class="">
+        <h2 class="font-bold">Synonym</h2>
+        <div class="">${createElements(word.synonyms)}</div>
+    </div>
+    `
+    document.getElementById("word_modal").showModal()
 }
 
 const displayLevelWord = (words) => {
@@ -54,6 +116,7 @@ const displayLevelWord = (words) => {
             <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
         </div>
         `
+        manageSpinner(false)
         return
     }
 
@@ -79,7 +142,7 @@ const displayLevelWord = (words) => {
                 <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
                     <i class="fa-solid fa-circle-info"></i>
                 </button>
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+                <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
                     <i class="fa-solid fa-volume-high"></i>
                 </button>
             </div>
@@ -88,6 +151,7 @@ const displayLevelWord = (words) => {
         wordContainter.append(card)
     })
     // console.log(words)
+    manageSpinner(false)
 }
 
 const displayLesson = (lessons) => {
@@ -114,3 +178,22 @@ const displayLesson = (lessons) => {
 }
 
 loadLessons()
+
+document.getElementById("btn-search").addEventListener("click", () => {
+    removeActive()
+    const input = document.getElementById("input-search")
+    const searchValue = input.value.trim().toLowerCase()
+    console.log(searchValue)
+
+    fetch("https://openapi.programming-hero.com/api/words/all")
+        .then(res => res.json())
+        .then(data => {
+            const allWords = data.data 
+            // console.log(allWords)
+            const filterWords = allWords.filter(word =>
+                word.word.toLowerCase().includes(searchValue)
+            )
+        // console.log(filterWords)
+        displayLevelWord(filterWords)
+        })
+})
